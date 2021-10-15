41c3705ce63f6a93db2f1e2543cc96d5cdeace58
sblee9126 <46069735+subin9126@users.noreply.github.com>
2021-10-15 15:22:41 +0900

    Delete subin_final_scripts_KUH directory

--- subin_final_scripts_KUH/pipeline_i_Resize_phstissue_for_QSMnet.m
+++
@@ -1,9 +0,0 @@
-
-file = dir([dirs.phs_tissue '\' subj suffix.mat '.mat']);
-    fprintf('--Rewriting subject %s --\n', subj)
-    
-    load([dirs.phs_tissue '\' file.name]);
-    phs_tissue_zp_undo = phs_tissue((1+zp_x):(end-zp_x), (1+zp_y):(end-zp_y), (1+zp_z):(end-zp_z));
-    save([dirs.phs_tissue '\' file.name], 'phs_tissue', 'phs_tissue_zp_undo', 'zp_x', 'zp_y', 'zp_z'); 
-    clear phs_tissue phs_tissue_zp_undo
-    
